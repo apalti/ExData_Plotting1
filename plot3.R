d63e8c9f3b856151fa651e7b12ab78afebc2fb91
Amit <amit.palti@gmail.com>
2018-08-27 19:25:10 +0300

    Now with legend

--- plot3.R
+++ plot3.R
@@ -21,7 +21,8 @@ T <- na.omit(subset(T,select=c(posTime,Sub_metering_1,Sub_metering_2,Sub_meterin
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
 # plotting the graph
 plot(T$posTime,T$Sub_metering_1,type="l",xlab="",ylab="Energy sub metering")
-lines(T$posTime,T$Sub_metering_2,type="l")
-lines(T$posTime,T$Sub_metering_3,type="l")
+lines(T$posTime,T$Sub_metering_2,type="l",col="red")
+lines(T$posTime,T$Sub_metering_3,type="l",col="blue")
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col= c("black","red","blue"), lty=1, cex=0.8)
 #closing the device
 dev.off()
